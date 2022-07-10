dd55a323857ab42888271e46ec8410777a4190cd
Uno0306 <zion6878@naver.com>
2022-07-10 16:37:14 +0900

    npm start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
+import React from 'react';
 import { Link, Outlet, NavLink } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Domain from './components/Domain';
 
 function App() {
   return (
